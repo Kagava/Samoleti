c8aca63869de1ee0317650a3aabce967757387db
Kagava <esspakov@mial.ru>
2023-05-01 00:41:13 +0700

    Добавьте файлы проекта.

---
+++ Samoleti/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Samoleti.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
---
+++ Samoleti/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace Samoleti.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
